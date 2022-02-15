79cb6707e71f1f6cc77a0561546284b2f76d913c
unknown <shukumar-542>
2022-02-15 12:50:08 +0600

    react props

--- src/App.js
+++ src/App.js
@@ -2,24 +2,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const players = ['warner','smith','Kohli','rohit'];
+  const products =[
+    {name:'Photoshop', price:"$5.99"},
+    {name:'PDF Reader', price:"$2.99"},
+    {name:'Illastrator', price:"$5.32"}
+  ]
+
+
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ul>
+        {
+          players.map(player => <li>{player}</li>)
+        }
+        {
+          products.map(product => <li>{product.name}</li>)
+        }
+        {
+          products.map(product => <Products product ={product}></Products>)
+        }
+      </ul>
+        
+        
+        <Crickter name="Rohit Sharma" country ="India"></Crickter>
+        <Crickter name = "Gallen Maxwell" country = "Australia"></Crickter>
+        <Crickter name = "jos Buttler" country ="England"></Crickter>
+
+        
       </header>
     </div>
   );
+}
+
+function Crickter(props){
+  const styles = {
+    border: '1px solid green',
+    margin: '10px'
+  }
+  return (
+    <div style={styles}>
+      <h1>Name :{props.name} </h1>
+      <p>country: {props.country} </p>
+    </div>
+  );
+}
+
+function Products(props){
+  const productstyle ={
+    backgroundColor:"lightgray",
+    border: '1px solid gray',
+    borderRadius:'5px',
+    height:'200px',
+    width:'200px',
+    
+
+
+  }
+  const {name, price} =props.product
+  return(
+    <div style={productstyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+
+    </div>
+  )
 }
 
 export default App;
