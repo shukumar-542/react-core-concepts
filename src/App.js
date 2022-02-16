6b3108a7131a67ee3662ca3c74c320dfc8e2b52f
unknown <shukumar-542>
2022-02-16 13:00:17 +0600

    using useState Component useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -9,11 +10,19 @@ function App() {
     {name:'Illastrator', price:"$5.32"}
   ]
 
+  const friends =[
+    { name: 'james anderson' , address :'california'},
+    { name: 'adam smith', address:'washington dc'},
+    { name :'jemse carby', address:'weales'}
+
+  ]
+
 
  
   return (
     <div className="App">
       <header className="App-header">
+      <Counter></Counter>
       <ul>
         {
           players.map(player => <li>{player}</li>)
@@ -26,10 +35,17 @@ function App() {
         }
       </ul>
         
+        <ul>
+          {
+            friends.map(friend =><Friends friend={friend}></Friends>)
+          }
+        </ul>
+        <Users></Users>
         
         <Crickter name="Rohit Sharma" country ="India"></Crickter>
         <Crickter name = "Gallen Maxwell" country = "Australia"></Crickter>
         <Crickter name = "jos Buttler" country ="England"></Crickter>
+        
 
         
       </header>
@@ -72,4 +88,50 @@ function Products(props){
   )
 }
 
+function Friends(props){
+ 
+  const{name, address} = props.friend;
+  return(
+    <div>
+        <h1>{name}</h1>
+        <h3>{address}</h3>
+    </div>
+  );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const handleIncrese = ()=>{
+    // const newCount = count +1;
+    setCount(count + 1);
+  }
+  
+  return(
+    <div>
+      <h1> Count :{count}</h1>
+      <button onClick={handleIncrese}>Increse</button>
+
+      <button onClick={()=>setCount(count-1)}>Decrese</button>
+    </div>
+  )
+}
+function Users(){
+
+  const [users,setUser] = useState([]) 
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>setUser(data))
+  })
+  return(
+    <div>
+      <h3>Dynamic users {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
 export default App;
